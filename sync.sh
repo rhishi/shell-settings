afcc67c0fae288748eed5a4e78f7b640f5bef322
Rhishikesh Limaye <rlimaye@google.com>
2018-01-19 10:37:01 -0800

    Extend sync.sh with dry-run, run, help options

--- sync.sh
+++ sync.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-rsync -a --progress \
+arg="$1"
+progname=`basename "$0"`
+
+usage="  Usage:
+     $progname -h|--help
+  or $progname -d|--dry-run
+  or $progname -r|--run"
+
+options="-a --progress"
+
+case "$arg" in
+  -d|--dry-run|--dryrun) options="$options --dry-run" ;;
+  -r|--run) ;;
+  -h|--help|*) echo "$usage"; exit 0 ;;
+esac
+
+set -x
+rsync $options \
   --exclude='.git*' \
   --exclude='*~' \
   --exclude='sync.sh' \
-  . ~ \
-  # --dry-run
+  . ~
